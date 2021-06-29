00e4470dd305481ad7a41e1b2b445b0f93dcef23
Wiley <wileycorning@gmail.com>
2021-06-29 11:23:43 -0500

    Add initial support for IntArray, BitVec32

---
+++ src/csharp/ParserLibrary/Syntax/Identifiers/SmtBitVec32.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Semgus.Syntax {
+    public struct SmtBitVec32 : IEquatable<SmtBitVec32> {
+        public readonly System.UInt32 value;
+
+        public SmtBitVec32(uint value) {
+            this.value = value;
+        }
+
+        public override bool Equals(object obj) => obj is SmtBitVec32 vec && Equals(vec);
+        public bool Equals(SmtBitVec32 other) => value == other.value;
+
+        public override int GetHashCode() => value.GetHashCode();
+
+        public static bool operator ==(SmtBitVec32 left, SmtBitVec32 right) => left.Equals(right);
+        public static bool operator !=(SmtBitVec32 left, SmtBitVec32 right) => !(left == right);
+
+        public override string ToString() => "#x" + Convert.ToString(value, 16).PadLeft(8, '0');
+    }
+}
---
+++ src/csharp/ParserLibrary/Syntax/Identifiers/SmtIntArray.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Semgus.Syntax {
+    public class SmtIntArray : IEquatable<SmtIntArray> {
+        private readonly Dictionary<int, int> _map;
+
+        public SmtIntArray(IReadOnlyList<int> data) {
+            var map = new Dictionary<int, int>();
+            for (int i = 0; i < data.Count; i++) map[i] = data[i];
+            _map = map;
+        }
+
+        public SmtIntArray() {
+            _map = new Dictionary<int, int>();
+        }
+
+        // Private constructor because this wraps its input argument without cloning
+        private SmtIntArray(Dictionary<int, int> map) {
+            _map = map;
+        }
+
+        public SmtIntArray Store(int idx, int value) {
+            var copy = new Dictionary<int, int>(_map);
+            copy[idx] = value;
+            return new SmtIntArray(copy);
+        }
+
+        public int Select(int idx) {
+            if (_map.TryGetValue(idx, out var value)) {
+                return value;
+            } else {
+#if RETURN_ZERO_ON_KEY_MISS
+                _map[idx] = 0;
+                return 0;
+#else
+                throw new KeyNotFoundException();
+#endif
+            }
+        }
+
+        public override bool Equals(object obj) => Equals(obj as SmtIntArray);
+
+        public bool Equals(SmtIntArray other) =>
+            (!(other is null)) &&
+            (_map.Count == other._map.Count) &&
+            other._map.All(kvp => _map.TryGetValue(kvp.Key, out var v) && kvp.Value == v);
+
+        public override int GetHashCode() {
+            var hc = 0;
+            foreach (var kvp in _map) {
+                hc = HashCode.Combine(hc, kvp.Key, kvp.Value);
+            }
+            return hc;
+        }
+
+        public static bool operator ==(SmtIntArray left, SmtIntArray right) => (left is null && right is null) || left.Equals(right);
+
+        public static bool operator !=(SmtIntArray left, SmtIntArray right) => !(left == right);
+    }
+}
