fb40d5d541f188e40754dbcc3b6f254a5e0d9121
kjcjohnson <kjcjohnson@ymail.com>
2022-04-11 17:45:18 -0500

    Caveats and considerations for initial release (#62)
    * Update documentation on ISmtTheory
    
    * Update README with considerations
    
    * Update package settings
    
    * Note about reaching out for desired features
    
    * Add badges to README

--- Semgus-Lib/Model/Smt/ISmtTheory.cs
+++ Semgus-Lib/Model/Smt/ISmtTheory.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace Semgus.Model.Smt
 {
+    /// <summary>
+    /// A particular theory
+    /// </summary>
     public interface ISmtTheory
     {
+        /// <summary>
+        /// The name of this theory
+        /// </summary>
         SmtIdentifier Name { get; }
+
+        [Obsolete("This member will be updated or removed to support additional theories. Use SmtContext to look up specific sorts.")]
         IReadOnlyDictionary<SmtIdentifier, SmtSort> Sorts { get; }
+
+        [Obsolete("This member will be updated or removed to support additional theories. Use SmtContext to look up specific functions.")]
         IReadOnlyDictionary<SmtIdentifier, SmtFunction> Functions { get; }
     }
 }
