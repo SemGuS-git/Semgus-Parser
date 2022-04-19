45515cc2b9e4c742dba7474aa0a10434e601df4b
Keith Johnson <kjcjohnson@ymail.com>
2022-04-19 10:28:57 -0500

    Documentation and cleanup of sort class

--- Semgus-Lib/Model/Smt/SmtSort.cs
+++ Semgus-Lib/Model/Smt/SmtSort.cs
@@ -7,13 +7,23 @@ using System.Threading.Tasks;
 
 namespace Semgus.Model.Smt
 {
+    /// <summary>
+    /// Base class for all term sorts
+    /// </summary>
     public abstract class SmtSort
     {
+        /// <summary>
+        /// Constructs a new sort with the given name
+        /// </summary>
+        /// <param name="name">Sort name</param>
         public SmtSort(SmtSortIdentifier name)
         {
             Name = name;
         }
 
+        /// <summary>
+        /// Name of this sort
+        /// </summary>
         public SmtSortIdentifier Name { get; }
 
         /// <summary>
@@ -26,17 +36,24 @@ namespace Semgus.Model.Smt
         /// </summary>
         public bool IsSortParameter { get; protected set; } = false;
 
-        private class GenericSort : SmtSort
-        {
-            public GenericSort(SmtSortIdentifier name) : base(name)
-            { }
-        }
-
+        /// <summary>
+        /// A sort containing wildcard parameters. Useful in rank templates.
+        /// Indices in the sort name may be '*', which will match anything.
+        /// </summary>
         public class WildcardSort : SmtSort
         {
+            /// <summary>
+            /// Constructs a new wildcard sort
+            /// </summary>
+            /// <param name="name">Sort name with wildcards</param>
             public WildcardSort(SmtSortIdentifier name) : base(name)
             { }
 
+            /// <summary>
+            /// Checks if this sort matches another sort (taking wildcards into account)
+            /// </summary>
+            /// <param name="other">The other sort</param>
+            /// <returns>True if matches, false otherwise</returns>
             public bool Matches(SmtSort other)
             {
                 if (Name.Name.Symbol == other.Name.Name.Symbol
@@ -56,27 +73,56 @@ namespace Semgus.Model.Smt
             }
         }
 
+        /// <summary>
+        /// Factory for constructing unique sorts (e.g., for rank templates)
+        /// </summary>
         public class UniqueSortFactory
         {
+            /// <summary>
+            /// Unique sorts that are different from any other
+            /// </summary>
             private class UniqueSort : SmtSort
             {
+                /// <summary>
+                /// Constructs a new sort with the given prefix and counter
+                /// </summary>
+                /// <param name="prefix"></param>
+                /// <param name="counter"></param>
                 public UniqueSort(string prefix, long counter) : base(new SmtSortIdentifier($"{prefix}{counter}"))
                 {
                     IsSortParameter = true;
                 }
             }
             
+            /// <summary>
+            /// Counter for unique sorts. Changed after constructing a sort
+            /// </summary>
             private static long _counter = 1;
 
+            /// <summary>
+            /// Prefix for this factory
+            /// </summary>
             private readonly string _prefix;
+
+            /// <summary>
+            /// Constructs a new UniqueSortFactory with the given prefix
+            /// </summary>
+            /// <param name="name">Sort prefix to use</param>
             public UniqueSortFactory(string name = "X")
             {
                 _prefix = name;
                 Sort = Next();
             }
 
+            /// <summary>
+            /// The most recently constructed sort
+            /// </summary>
             public SmtSort Sort { get; private set; }
 
+            /// <summary>
+            /// Constructs a new unique sort
+            /// </summary>
+            /// <returns>The constructed sort</returns>
             public SmtSort Next()
             {
                 Sort = new UniqueSort(_prefix, Interlocked.Increment(ref _counter));
