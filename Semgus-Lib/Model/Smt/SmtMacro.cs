46c2a682f9324ae2d798b23002880926a1884ff2
Keith Johnson <kjcjohnson@ymail.com>
2022-07-20 22:14:39 -0500

    Template for macro expansion

---
+++ Semgus-Lib/Model/Smt/SmtMacro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semgus.Model.Smt
+{
+    /// <summary>
+    /// Function-like object that can be expanded into SMT expressions
+    /// </summary>
+    public class SmtMacro : IApplicable
+    {
+        public SmtIdentifier Name => throw new NotImplementedException();
+
+        public ISmtTheory Theory => throw new NotImplementedException();
+
+        public string GetRankHelp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsArityPossible(int arity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryResolveRank([NotNullWhen(true)] out SmtFunctionRank? rank, SmtSort? returnSort, params SmtSort[] argumentSorts)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Semgus-Lib/Model/Smt/Transforms/SmtMacroExpander.cs
@@ -0,0 +1,35 @@
+﻿using Semgus.Model.Smt.Terms;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semgus.Model.Smt.Transforms
+{
+    public class SmtMacroExpander : SmtTermWalker<object?>
+    {
+        public SmtMacroExpander() : base(default(object))
+        {
+        }
+
+        public override (SmtTerm, object?) OnFunctionApplication(SmtFunctionApplication appl, IReadOnlyList<SmtTerm> arguments, IReadOnlyList<object?> up)
+        {
+            var applicable = appl.Definition;
+            if (applicable is SmtMacro macro)
+            {
+                return (MacroExpand(macro, appl.Rank, appl.Arguments), up);
+            }
+            else
+            {
+                return base.OnFunctionApplication(appl, arguments, up);
+            }
+        }
+
+        private SmtTerm MacroExpand(SmtMacro macro, SmtFunctionRank rank, IReadOnlyList<SmtTerm> args)
+        {
+            return null!;
+        }
+    }
+}
