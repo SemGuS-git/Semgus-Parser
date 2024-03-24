f9f305947db9743de30acf48dc3f0064b68ae335
Keith Johnson <kjcjohnson@ymail.com>
2024-03-24 11:28:18 -0500

    Annotations on sexpr output

--- SemgusParser/Sexpr/SmtTermWriter.cs
+++ SemgusParser/Sexpr/SmtTermWriter.cs
@@ -18,73 +18,117 @@ namespace Semgus.Parser.Sexpr
             _sw = sw;
         }
 
+        /// <summary>
+        /// Wraps an annotation block around the current term, if necessary
+        /// </summary>
+        /// <param name="term">Term to write annotations for, if any</param>
+        /// <param name="inner">Action to call for writing the term</param>
+        private void MaybeWriteAnnotations(SmtTerm term, Action inner)
+        {
+            if (term.Annotations != null && term.Annotations.Count > 0)
+            {
+                _sw.WriteList(() =>
+                {
+                    _sw.WriteSymbol("annotated");
+                    inner();
+                    foreach (var ann in term.Annotations)
+                    {
+                        _sw.WriteString(ann.Keyword.Name);
+                        _sw.Write(ann.Value);
+                    }
+                });
+            }
+            else
+            {
+                inner();
+            }
+        }
+
         public ISexprWriter VisitBitVectorLiteral(SmtBitVectorLiteral bitVectorLiteral)
         {
-            _sw.WriteBitVector(bitVectorLiteral.Value);
+            MaybeWriteAnnotations(bitVectorLiteral, () =>
+            {
+                _sw.WriteBitVector(bitVectorLiteral.Value);
+            });
             return _sw;
         }
 
         public ISexprWriter VisitDecimalLiteral(SmtDecimalLiteral decimalLiteral)
         {
-            _sw.WriteDecimal(decimalLiteral.Value);
+            MaybeWriteAnnotations(decimalLiteral, () =>
+            {
+                _sw.WriteDecimal(decimalLiteral.Value);
+            });
             return _sw;
         }
 
         public ISexprWriter VisitExistsBinder(SmtExistsBinder existsBinder)
         {
-            _sw.WriteList(() =>
+            MaybeWriteAnnotations(existsBinder, () =>
             {
-                _sw.WriteSymbol("exists");
-                _sw.WriteKeyword("bindings");
-                _sw.WriteList(existsBinder.NewScope.LocalBindings, b => _sw.Write(b.Id));
-                _sw.WriteKeyword("binding-sorts");
-                _sw.WriteList(existsBinder.NewScope.LocalBindings, b => _sw.Write(b.Sort.Name));
-                _sw.WriteKeyword("child");
-                existsBinder.Child.Accept(this);
+                _sw.WriteList(() =>
+                {
+                    _sw.WriteSymbol("exists");
+                    _sw.WriteKeyword("bindings");
+                    _sw.WriteList(existsBinder.NewScope.LocalBindings, b => _sw.Write(b.Id));
+                    _sw.WriteKeyword("binding-sorts");
+                    _sw.WriteList(existsBinder.NewScope.LocalBindings, b => _sw.Write(b.Sort.Name));
+                    _sw.WriteKeyword("child");
+                    existsBinder.Child.Accept(this);
+                });
             });
             return _sw;
         }
 
         public ISexprWriter VisitForallBinder(SmtForallBinder forallBinder)
         {
-            _sw.WriteList(() =>
+            MaybeWriteAnnotations(forallBinder, () =>
             {
-                _sw.WriteSymbol("forall");
-                _sw.WriteKeyword("bindings");
-                _sw.WriteList(forallBinder.NewScope.LocalBindings, b => _sw.Write(b.Id));
-                _sw.WriteKeyword("binding-sorts");
-                _sw.WriteList(forallBinder.NewScope.LocalBindings, b => _sw.Write(b.Sort.Name));
-                _sw.WriteKeyword("child");
-                forallBinder.Child.Accept(this);
+                _sw.WriteList(() =>
+                {
+                    _sw.WriteSymbol("forall");
+                    _sw.WriteKeyword("bindings");
+                    _sw.WriteList(forallBinder.NewScope.LocalBindings, b => _sw.Write(b.Id));
+                    _sw.WriteKeyword("binding-sorts");
+                    _sw.WriteList(forallBinder.NewScope.LocalBindings, b => _sw.Write(b.Sort.Name));
+                    _sw.WriteKeyword("child");
+                    forallBinder.Child.Accept(this);
+                });
             });
             return _sw;
         }
 
         public ISexprWriter VisitFunctionApplication(SmtFunctionApplication functionApplication)
         {
-            _sw.WriteList(() =>
+            MaybeWriteAnnotations(functionApplication, () =>
             {
-                _sw.WriteSymbol("application");
-                _sw.Write(functionApplication.Definition.Name);
-                _sw.WriteKeyword("argument-sorts");
-                _sw.WriteList(functionApplication.Rank.ArgumentSorts, s => _sw.Write(s.Name));
-                _sw.WriteKeyword("arguments");
-                _sw.WriteList(functionApplication.Arguments, a => a.Accept(this));
-                _sw.WriteKeyword("return-sort");
-                _sw.Write(functionApplication.Rank.ReturnSort.Name);
+                _sw.WriteList(() =>
+                {
+                    _sw.WriteSymbol("application");
+                    _sw.Write(functionApplication.Definition.Name);
+                    _sw.WriteKeyword("argument-sorts");
+                    _sw.WriteList(functionApplication.Rank.ArgumentSorts, s => _sw.Write(s.Name));
+                    _sw.WriteKeyword("arguments");
+                    _sw.WriteList(functionApplication.Arguments, a => a.Accept(this));
+                    _sw.WriteKeyword("return-sort");
+                    _sw.Write(functionApplication.Rank.ReturnSort.Name);
+                });
             });
             return _sw;
         }
 
         public ISexprWriter VisitLambdaBinder(SmtLambdaBinder lambdaBinder)
         {
-            _sw.WriteList(() =>
+            MaybeWriteAnnotations(lambdaBinder, () =>
             {
-                _sw.WriteSymbol("lambda");
-                _sw.WriteKeyword("arguments");
-                _sw.WriteList(lambdaBinder.ArgumentNames, an => _sw.Write(an));
-                _sw.WriteKeyword("body");
-                lambdaBinder.Child.Accept(this);
+                _sw.WriteList(() =>
+                {
+                    _sw.WriteSymbol("lambda");
+                    _sw.WriteKeyword("arguments");
+                    _sw.WriteList(lambdaBinder.ArgumentNames, an => _sw.Write(an));
+                    _sw.WriteKeyword("body");
+                    lambdaBinder.Child.Accept(this);
+                });
             });
             return _sw;
         }
@@ -96,59 +140,68 @@ namespace Semgus.Parser.Sexpr
 
         public ISexprWriter VisitMatchBinder(SmtMatchBinder matchBinder)
         {
-            _sw.WriteList(() =>
+            MaybeWriteAnnotations(matchBinder, () =>
             {
-                _sw.WriteSymbol("binder");
-                _sw.WriteKeyword("operator");
-                if (matchBinder.Constructor is null)
+                _sw.WriteList(() =>
                 {
-                    _sw.WriteNil();
-                }
-                else
-                {
-                    _sw.Write(matchBinder.Constructor.Name);
-                }
-                _sw.WriteKeyword("arguments");
-                _sw.WriteList(matchBinder.Bindings, b => _sw.Write(b.Binding.Id));
-                _sw.WriteKeyword("child");
-                matchBinder.Child.Accept(this);
+                    _sw.WriteSymbol("binder");
+                    _sw.WriteKeyword("operator");
+                    if (matchBinder.Constructor is null)
+                    {
+                        _sw.WriteNil();
+                    }
+                    else
+                    {
+                        _sw.Write(matchBinder.Constructor.Name);
+                    }
+                    _sw.WriteKeyword("arguments");
+                    _sw.WriteList(matchBinder.Bindings, b => _sw.Write(b.Binding.Id));
+                    _sw.WriteKeyword("child");
+                    matchBinder.Child.Accept(this);
+                });
             });
             return _sw;
         }
 
         public ISexprWriter VisitMatchGrouper(SmtMatchGrouper matchGrouper)
         {
-            _sw.WriteList(() =>
+            MaybeWriteAnnotations(matchGrouper, () =>
             {
-                _sw.WriteSymbol("match");
-                _sw.WriteKeyword("term");
-                matchGrouper.Term.Accept(this);
-                _sw.WriteKeyword("binders");
-                _sw.WriteList(matchGrouper.Binders, b => b.Accept(this));
+                _sw.WriteList(() =>
+                {
+                    _sw.WriteSymbol("match");
+                    _sw.WriteKeyword("term");
+                    matchGrouper.Term.Accept(this);
+                    _sw.WriteKeyword("binders");
+                    _sw.WriteList(matchGrouper.Binders, b => b.Accept(this));
+                });
             });
             return _sw;
         }
 
         public ISexprWriter VisitNumeralLiteral(SmtNumeralLiteral numeralLiteral)
         {
-            _sw.WriteNumeral(numeralLiteral.Value);
+            MaybeWriteAnnotations(numeralLiteral, () => { _sw.WriteNumeral(numeralLiteral.Value); });
             return _sw;
         }
 
         public ISexprWriter VisitStringLiteral(SmtStringLiteral stringLiteral)
         {
-            _sw.WriteString(stringLiteral.Value);
+            MaybeWriteAnnotations(stringLiteral, () => { _sw.WriteString(stringLiteral.Value); });
             return _sw;
         }
 
         public ISexprWriter VisitVariable(SmtVariable variable)
         {
-            _sw.WriteList(() =>
+            MaybeWriteAnnotations(variable, () =>
             {
-                _sw.WriteSymbol("variable");
-                _sw.Write(variable.Name);
-                _sw.WriteKeyword("sort");
-                _sw.Write(variable.Sort.Name);
+                _sw.WriteList(() =>
+                {
+                    _sw.WriteSymbol("variable");
+                    _sw.Write(variable.Name);
+                    _sw.WriteKeyword("sort");
+                    _sw.Write(variable.Sort.Name);
+                });
             });
             return _sw;
         }
